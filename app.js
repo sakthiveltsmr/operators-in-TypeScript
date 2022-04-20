dc3865428ce2c76046a195eaab72a48124285444
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-20 10:42:32 +0530

    spread

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+//Rest Operator
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var displayColors = function (message) {
     var colors = [];
     for (var _i = 1; _i < arguments.length; _i++) {
@@ -12,3 +22,16 @@ var displayColors = function (message) {
 var message = "list of colors";
 // let colors = ["red", "green", "blue"];
 displayColors(message, "red", "green", "blue");
+//spread operator
+var displayspreed = function (message) {
+    var colors1 = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        colors1[_i - 1] = arguments[_i];
+    }
+    console.log(message);
+    for (var j in colors1) {
+        console.log(colors1[j]);
+    }
+};
+var colors1 = ["yellow", "orange", "white"];
+displayspreed.apply(void 0, __spreadArray([message], colors1, false));
--- app.ts
+++ app.ts
@@ -15,3 +15,16 @@ let message = "list of colors";
 // let colors = ["red", "green", "blue"];
 
 displayColors(message, "red", "green", "blue");
+
+//spread operator
+
+let displayspreed = function (message, ...colors1) {
+  console.log(message);
+  for (let j in colors1) {
+    console.log(colors1[j]);
+  }
+};
+
+let colors1 = ["yellow", "orange", "white"];
+
+displayspreed(message, ...colors1);
